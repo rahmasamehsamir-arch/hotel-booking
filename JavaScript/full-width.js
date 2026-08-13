fa05c76b82279c63e136a05d79318f4ed01620f7
omar-maher9 <omarmaher66111@gmail.com>
2026-08-13 11:37:09 +0300

    Add click event to final read buttons

--- JavaScript/full-width.js
+++ JavaScript/full-width.js
@@ -1,1 +1,9 @@
 
+var buttons = document.querySelectorAll(".final-read-button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+        alert("More information coming soon!");
+    });
+});
