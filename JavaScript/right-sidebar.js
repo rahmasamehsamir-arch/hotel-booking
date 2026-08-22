14aefc48f37dfc9ef8768e636645b0621da8b712
omar-maher9 <omarmaher66111@gmail.com>
2026-08-22 03:43:53 +0300

    Update right-sidebar.js

--- JavaScript/right-sidebar.js
+++ JavaScript/right-sidebar.js
@@ -1,8 +1,7 @@
-// Array of image URLs for the slider
 var images = [
   "images/f.jpg",
   "images/g.jpg",
-  "../images/h.jpg"
+  "images/h.jpg"
 ];
 
 var currentIndex = 0;
@@ -11,14 +10,12 @@ var sliderImg = document.getElementById("slider-img");
 var prevBtn = document.getElementById("prev-btn");
 var nextBtn = document.getElementById("next-btn");
 
-// Previous button 
 
 prevBtn.addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + images.length) % images.length;
   sliderImg.src = images[currentIndex];
 });
 
-// Next button 
 
 nextBtn.addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % images.length;
